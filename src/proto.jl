663d9b6565531263ca9a441bffb34404e37ad130
Lyndon White <lyndon.white@invenialabs.co.uk>
2019-09-24 10:52:27 +0100

    create prototype

---
+++ src/proto.jl
@@ -0,0 +1,48 @@
+using Pkg: @pkg_str
+pkg"activate ."
+
+#---
+
+using Cassette
+#using JuliaInterpreter
+#using MagneticReadHead
+using Debugger
+
+#---
+
+function eg1()
+    println("1a")
+    eg2()
+    println("1b")
+end
+function eg2()
+    println("  2a")
+    eg3()
+    println("  2b")
+end
+function eg3()
+    x=1+1+1
+    print("    ")
+    println(x)
+end
+eg1()
+
+#---
+
+Cassette.@context MixModeDebugCtx
+function Cassette.overdub(::MixModeDebugCtx, ::typeof(eg2))
+    Core.println("-- begin interpretting")
+    try
+        Debugger.@run eg2()
+    finally
+        Core.println("-- end interpretting")
+    end
+end
+
+#---
+
+bp = @breakpoint eg3()
+
+Cassette.recurse(MixModeDebugCtx(), eg1)
+
+Debugger.@run eg3()
