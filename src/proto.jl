b7e9ed52025621530b0fc7dd79bda1835ceac50a
Lyndon White <lyndon.white@invenialabs.co.uk>
2019-09-25 11:03:34 +0100

    MRH 0.1 style

--- src/proto.jl
+++ src/proto.jl
@@ -4,8 +4,8 @@ pkg"activate ."
 #---
 
 using Cassette
-#using JuliaInterpreter
-#using MagneticReadHead
+using JuliaInterpreter: JuliaInterpreter, BreakpointSignature
+using MagneticReadHead
 using Debugger
 
 #---
@@ -27,20 +27,48 @@ function eg3()
 end
 eg1()
 
+
 #---
 
 Cassette.@context MixModeDebugCtx
-function Cassette.overdub(::MixModeDebugCtx, ::typeof(eg2))
-    Core.println("-- begin interpretting")
-    try
-        Debugger.@run eg2()
-    finally
-        Core.println("-- end interpretting")
+
+function setup_overdubs(breakpoints=JuliaInterpreter._breakpoints)
+    for bp in breakpoints
+        create_overdub(bp)
     end
 end
 
-#---
+function create_overdub(bp)
+    ft = MagneticReadHead.functiontypeof(bp.f)
+    #TODO: select based on all the method sig info, not just the function
+    @eval function Cassette.overdub(::MixModeDebugCtx, f::$ft, args...)
+        Core.println("-- begin interpretting")
+        try
+            # We wrap the call in a thunk before invoking it so that Debugger.jl
+            # can break on the call itself, if required.
+            # If we knew for sure would break then could use @enter
+            # TODO: remove this, but accessing Debugger internals for doing this
+            thunk() = f(args...)
+            Debugger.@run thunk()
+        finally
+            Core.println("-- end interpretting")
+        end
+    end
+end
+
+methodswith(typeof(eg3))
 
 bp = @breakpoint eg3()
-
+setup_overdubs()
 Cassette.recurse(MixModeDebugCtx(), eg1)
+
+##############
+using BenchmarkTools
+
+function summer(A)
+   s = zero(eltype(A))
+   for a in A
+       s += a
+   end
+   return s
+end
